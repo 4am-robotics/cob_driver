c05b23da4f3f539e6ae310961d4090b5d8c1a627
b-it-bots <b-it-bots@cob3-1-pc1.(none)>
2010-03-19 11:01:44 +0100

    running java node with queuing subscriber

--- cob_lbr/ros/src/cob_lbr.java
+++ cob_lbr/ros/src/cob_lbr.java
@@ -2,6 +2,7 @@ import ros.*;
 import ros.communication.*;
 import ros.pkg.std_msgs.msg.String;
 import ros.pkg.cob_msgs.msg.JointCommand;
+import ros.pkg.trajectory_msgs.msg.JointTrajectory;
 
 
 class cob_lbr {
@@ -15,27 +16,75 @@ class cob_lbr {
 
         NodeHandle n = ros.createNodeHandle();
 
-        try
-        {
-            Publisher<ros.pkg.std_msgs.msg.String> pub = n.advertise("/pub", new ros.pkg.std_msgs.msg.String(), 100);
-            ros.pkg.std_msgs.msg.String m = new ros.pkg.std_msgs.msg.String();
-            m.data = "Hello, ROS";
-            pub.publish(m);
-            try {
-                Thread.sleep(4000);
-            } catch (InterruptedException e)
-            {
-            }
-            pub.shutdown();
-        }
-        catch(ros.RosException e)
-        {
-            System.out.println("Exception");
-        }
+	    try
+	    {
+	        Publisher<ros.pkg.std_msgs.msg.String> pub = n.advertise("/sub", new ros.pkg.std_msgs.msg.String(), 100);
+	        ros.pkg.std_msgs.msg.String m = new ros.pkg.std_msgs.msg.String();
+	        m.data = "Hello, ROS";
+	        pub.publish(m);
+/*	        try {
+	            Thread.sleep(4000);
+	        } catch (InterruptedException e)
+	        {
+	        }
+	        pub.shutdown();
+	    }
+	    catch(ros.RosException e)
+	    {
+	        System.out.println("Exception");
+	    }
 
+		try
+		{
+*/
+			ros.logInfo("inside try");
+			Subscriber.QueueingCallback<ros.pkg.std_msgs.msg.String> callback = new Subscriber.QueueingCallback<ros.pkg.std_msgs.msg.String>(); 
+			Subscriber<ros.pkg.std_msgs.msg.String> sub = n.subscribe("/sub", new ros.pkg.std_msgs.msg.String(), callback, 10);
 
-	// Definiere eine Zeichenkette.
-	
-	// Gebe die Zeichenkette an der Konsole aus.
-	}
-}
+			int i = 0;
+			while (i<50)
+			{
+			    m.data = "Hello, ROS";
+			    pub.publish(m);
+	        
+				ros.logInfo("spin");
+				n.spinOnce();
+
+				try 
+				{
+					ros.logInfo("sleep1");
+			    	Thread.sleep(100);
+			    }
+			    catch (InterruptedException e)
+			    {
+		        }
+
+				ros.logInfo("while");
+				while (!callback.isEmpty())
+				{
+					ros.logInfo("callback");
+					System.out.println(callback.pop().data);
+				}
+				i = i+1;
+			}
+			
+			try 
+			{
+				ros.logInfo("sleep2");
+	        	Thread.sleep(4000);
+	        }
+	        catch (InterruptedException e)
+	        {
+	        }
+
+			ros.logInfo("shutdown");
+			sub.shutdown();
+			pub.shutdown();
+
+		}//try
+		catch(Exception e)
+		{
+	        System.out.println("Exception");			
+		}
+	}//main
+}//class
