b711264d43ba41d753ce0fec9b2bd1a632d6490c
cob <cob@pc-cpc3.(none)>
2010-03-25 10:35:54 +0100

    Header copyright infos adapted

--- cob_relayboard/common/include/cob_relayboard/SerRelayBoard.h
+++ cob_relayboard/common/include/cob_relayboard/SerRelayBoard.h
@@ -1,22 +1,55 @@
-/* -----------------------------------------------------------------------------------
+/****************************************************************
  *
- *		Copyright (c) 2007 Neobotix (www.neobotix.de)
+ * Copyright (c) 2010
  *
- *      This software is allowed to be used and modified only in association with a Neobotix
- *      robot platform. It is allowed to include the software into applications and
- *      to distribute it with a Neobotix robot platform. 
- *      It is not allowed to distribute the software without a Neobotix robot platform. 
+ * Fraunhofer Institute for Manufacturing Engineering	
+ * and Automation (IPA)
  *
- *      This software is provided WITHOUT ANY WARRANTY; without even the
- *		implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- *		PURPOSE. See the Neobotix License (Version 1.0) for more details.
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
  *
- *      You should have received a copy of the Neobotix License
- *      along with this software; if not, write to the 
- *      Gesellschaft fuer Produktionssysteme, Neobotix, Nobelstrasse 12, 70569 Stuttgart, Germany
+ * Project name: care-o-bot
+ * ROS stack name: cob_driver
+ * ROS package name: cob_relayboard
+ * Description: Class for communication with relayboard. The relayboard is mainly used for reading the Emergencystop and Laserscannerstop states.
+ *								
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *			
+ * Author: Philipp Koehler
+ * Supervised by: Christian Connette
  *
- * -----------------------------------------------------------------------------------
- */
+ * Date of creation: March 2010
+ * ToDo:
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Fraunhofer Institute for Manufacturing 
+ *       Engineering and Automation (IPA) nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License LGPL for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License LGPL along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
 
 #ifndef SerRelayBoard_INCLUDEDEF_H
 #define SerRelayBoard_INCLUDEDEF_H
