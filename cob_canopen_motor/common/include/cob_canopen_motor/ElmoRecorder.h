09b3cd35720fd09ada7fe767a0fa4b3247550639
cob <cob@pc-cpc3.(none)>
2010-04-14 15:59:08 +0200

    ElmoRecorder.h hasn't been added to the index during merge

---
+++ cob_canopen_motor/common/include/cob_canopen_motor/ElmoRecorder.h
@@ -0,0 +1,71 @@
+/****************************************************************
+ *
+ * Copyright (c) 2010
+ *
+ * Fraunhofer Institute for Manufacturing Engineering	
+ * and Automation (IPA)
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Project name: care-o-bot
+ * ROS stack name: cob3_common
+ * ROS package name: canopen_motor
+ * Description: Contains functions to read out Elmo-Recorder and to process and give out accordant data.
+ *								
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *			
+ * Author: Philipp Köhler
+ * Supervised by: Christian Connette, email:christian.connette@ipa.fhg.de
+ *
+ * Date of creation: Mar 2010
+ * ToDo:
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Fraunhofer Institute for Manufacturing 
+ *       Engineering and Automation (IPA) nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as 
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License LGPL for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License LGPL along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
+
+#ifndef _ElmoRecorder_H
+#define _ElmoRecorder_H
+
+//#include <canopen_motor/CanDriveHarmonica.h>
+class CanDriveHarmonica;
+
+class ElmoRecorder {
+	public:
+		
+		ElmoRecorder(CanDriveHarmonica * pParentHarmonicaDrive);
+
+		~ElmoRecorder();
+		
+	private:
+		CanDriveHarmonica* pHarmonicaDrive;
+};
+
+#endif
