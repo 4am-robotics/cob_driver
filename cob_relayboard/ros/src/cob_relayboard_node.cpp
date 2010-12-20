3914061bc94717fb6d61e4055285e3093fc48300
cpc-pk <PhilKoehler@gmx.de>
2010-12-20 18:46:37 +0100

    edited Relayboard: now publishes only on topic, when succesfully connected to relayboard; when connection to relayboard breaks, publish EM_STOP_ACTIVE messages

--- cob_relayboard/ros/src/cob_relayboard_node.cpp
+++ cob_relayboard/ros/src/cob_relayboard_node.cpp
@@ -92,7 +92,10 @@ class NodeClass
 			topicPub_isEmergencyStop = n.advertise<cob_msgs::EmergencyStopState>("/emergency_stop_state", 1);
 
 			// Make sure member variables have a defined state at the beginning
-			EM_stop_status_ = ST_EM_FREE;
+			EM_stop_status_ = ST_EM_ACTIVE;
+			relayboard_available = false;
+			relayboard_online = false;
+			relayboard_timeout = 2.0;
 			duration_for_EM_free_ = ros::Duration(1);
 		}
         
@@ -112,6 +115,11 @@ class NodeClass
 		int EM_stop_status_;
 		ros::Duration duration_for_EM_free_;
 		ros::Time time_of_EM_confirmed_;
+		double relayboard_timeout;
+
+		ros::Time time_last_message_received_;
+		bool relayboard_online; //the relayboard is sending messages at regular time
+		bool relayboard_available; //the relayboard has sent at least one message -> publish topic
 
 		// possible states of emergency stop
 		enum
@@ -161,6 +169,8 @@ int NodeClass::init()
 		sComPort ="/dev/ttyUSB0";
 		ROS_WARN("ComPort Parameter not available, using default Port: %s",sComPort.c_str());
 	}
+
+	n.param("message_timeout", relayboard_timeout, 2.0);
     
 	m_SerRelayBoard = new SerRelayBoard(sComPort);
 	ROS_INFO("Opened Relayboard at ComPort = %s", sComPort.c_str());
@@ -168,7 +178,7 @@ int NodeClass::init()
 	m_SerRelayBoard->init();
 
 	// Init member variable for EM State
-	EM_stop_status_ = ST_EM_FREE;
+	EM_stop_status_ = ST_EM_ACTIVE;
 	duration_for_EM_free_ = ros::Duration(1);
 
 	return 0;
@@ -186,12 +196,18 @@ int NodeClass::requestBoardStatus() {
 	ret = m_SerRelayBoard->evalRxBuffer();
 	if(ret==SerRelayBoard::NOT_INITIALIZED) {
 		ROS_ERROR("Failed to read relayboard data over Serial, the device is not initialized");
+		relayboard_online = false;
 	} else if(ret==SerRelayBoard::NO_MESSAGES) {
 		ROS_ERROR("For a long time, no messages from RelayBoard have been received, check com port!");
+		if(time_last_message_received_.toSec() - ros::Time::now().toSec() > relayboard_timeout) {relayboard_online = false;}
 	} else if(ret==SerRelayBoard::TOO_LESS_BYTES_IN_QUEUE) {
 		//ROS_ERROR("Relayboard: Too less bytes in queue");
 	} else if(ret==SerRelayBoard::CHECKSUM_ERROR) {
 		ROS_ERROR("A checksum error occurred while reading from relayboard data");
+	} else if(ret==SerRelayBoard::NO_ERROR) {
+		relayboard_online = true;
+		relayboard_available = true;
+		time_last_message_received_ = ros::Time::now();
 	}
 
 	return 0;
@@ -251,7 +267,14 @@ void NodeClass::sendEmergencyStopStates()
 			break;
 		}
 	};
-	EM_msg.emergency_state = EM_stop_status_;
-	topicPub_isEmergencyStop.publish(EM_msg);
-}
 
+
+	if(relayboard_online == false) {
+		EM_stop_status_ = EM_msg.EMSTOP;
+	}
+
+	if(relayboard_available) {		
+		EM_msg.emergency_state = EM_stop_status_;
+		topicPub_isEmergencyStop.publish(EM_msg);
+	}
+}
