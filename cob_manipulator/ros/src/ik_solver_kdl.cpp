e0a045e5d8426314d1d655014fa7145014b5b877
ipa-taj-dm <daniel.ahti.maki@gmail.com>
2011-04-14 18:24:43 +0200

    modifications for wimicare project

--- cob_manipulator/ros/src/ik_solver_kdl.cpp
+++ cob_manipulator/ros/src/ik_solver_kdl.cpp
@@ -98,7 +98,7 @@ bool ik_solve(kinematics_msgs::GetPositionIK::Request  &req,
 	JntArray q(nj);
 	JntArray q_init(nj);
 	for(int i = 0; i < nj; i++)
-		q_init(i) = 0.0; //req.ik_request.ik_seed_state.joint_state.position[i];
+		q_init(i) = req.ik_request.ik_seed_state.joint_state.position[i];
 	Frame F_dest;
 	Frame F_ist;
 	fksolver1.JntToCart(q_init, F_ist);
