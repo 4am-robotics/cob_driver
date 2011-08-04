5c72a6727398d6c88bbccfdcca248431ab22340f
Jan Fischer <jsf@ipa.fhg.de>
2011-08-04 11:28:31 +0200

    PCH

---
+++ cob_camera_sensors/common/include/cob_camera_sensors/StdAfx.h
@@ -0,0 +1,29 @@
+#ifdef __LINUX__
+	// Void
+#else
+
+#include <iostream>
+#include <limits>
+#include <map>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <set>
+#include <windows.h>
+#include <deque>
+#include <math.h>
+#include <set>
+#include <assert.h>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/progress.hpp>
+#include <boost/math/distributions/binomial.hpp>
+
+#include <opencv/cv.h>
+#include <opencv/ml.h>
+#include <opencv/highgui.h>
+
+#include "cob_vision/windows/src/extern/TinyXml/tinyxml.h"
+#include "cob_vision/windows/src/extern/TinyXml/tinystr.h"
+
+#endif
