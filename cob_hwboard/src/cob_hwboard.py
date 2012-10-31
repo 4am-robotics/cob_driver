f76c00a4e26ea58a690d515c5dc84ca6746f7cea
ipa-uhr-eh <eduard.herkel@ipa.fraunhofer.de>
2012-10-31 11:12:04 +0100

    updated some error messages

--- cob_hwboard/src/cob_hwboard.py
+++ cob_hwboard/src/cob_hwboard.py
@@ -442,22 +442,22 @@ class HwBoard:
 						# mapping for temperature sensors
 						if read_id == self.head_sensor_param:
 							status_object.name = "Head Temperature"
-							status_object.hardware_id = "hcboard_channel " + str(send_channel)
+							status_object.hardware_id = "hwboard_channel " + str(send_channel)
 						elif read_id == self.eye_sensor_param:
 							status_object.name = "Eye Camera Temperature"
-							status_object.hardware_id = "hcboard_channel = " + str(send_channel)
+							status_object.hardware_id = "hwboard_channel = " + str(send_channel)
 						elif read_id == self.torso_module_sensor_param:
 							status_object.name = "Torso Module Temperature"
-							status_object.hardware_id = "hcboard_channel =" + str(send_channel)
+							status_object.hardware_id = "hwboard_channel =" + str(send_channel)
 						elif read_id == self.torso_sensor_param:
 							status_object.name = "Torso Temperature"
-							status_object.hardware_id = "hcboard_channel =" + str(send_channel)
+							status_object.hardware_id = "hwboard_channel =" + str(send_channel)
 						elif read_id == self.pc_sensor_param:	
 							status_object.name = "PC Temperature"
-							status_object.hardware_id = "hcboard_channel =" + str(send_channel)
+							status_object.hardware_id = "hwboard_channel =" + str(send_channel)
 						elif read_id == self.engine_sensor_param:
 							status_object.name = "Engine Temperature"
-							status_object.hardware_id = "hcboard_channel = " + str(send_channel)
+							status_object.hardware_id = "hwboard_channel = " + str(send_channel)
 						else:
 							level = 1
 							status_object.message = "cannot map if from yaml file to temperature sensor"
@@ -500,22 +500,22 @@ class HwBoard:
 
 						if send_channel == 0:
 							status_object.name = "Akku Voltage"						
-							status_object.hardware_id = "hcboard_channel = 0"
+							status_object.hardware_id = "hwboard_channel = 0"
 						elif send_channel == 1:
 							status_object.name = "Torso Engine Voltage"						
-							status_object.hardware_id = "hcboard_channel = 1"
+							status_object.hardware_id = "hwboard_channel = 1"
 						elif send_channel == 2:
 							status_object.name = "Torso Logic Voltage"						
-							status_object.hardware_id = "hcboard_channel = 2"
+							status_object.hardware_id = "hwboard_channel = 2"
 						elif send_channel == 3:
 							status_object.name = "Tray Logic Voltage"						
-							status_object.hardware_id = "hcboard_channel = 3"
+							status_object.hardware_id = "hwboard_channel = 3"
 						elif send_channel == 6:
 							status_object.name = "Arm Engine Voltage"						
-							status_object.hardware_id = "hcboard_channel = 6"
+							status_object.hardware_id = "hwboard_channel = 6"
 						elif send_channel == 7:
 							status_object.name = "Tray Engine Voltage"						
-							status_object.hardware_id = "hcboard_channel = 7"
+							status_object.hardware_id = "hwboard_channel = 7"
 
 					# set values for current parameters
 					else:
@@ -534,24 +534,24 @@ class HwBoard:
 
 						if send_channel == 1:
 							status_object.name = "Torso Engine Current"						
-							status_object.hardware_id = "hcboard_channel = 1"
+							status_object.hardware_id = "hwboard_channel = 1"
 						elif send_channel == 2:
 							status_object.name = "Torso Logic Current"						
-							status_object.hardware_id = "hcboard_channel = 2"
+							status_object.hardware_id = "hwboard_channel = 2"
 						elif send_channel == 3:
 							status_object.name = "Tray Logic Current"						
-							status_object.hardware_id = "hcboard_channel = 3"
+							status_object.hardware_id = "hwboard_channel = 3"
 						elif send_channel == 6:
 							status_object.name = "Arm Engine Current"						
-							status_object.hardware_id = "hcboard_channel = 6"
+							status_object.hardware_id = "hwboard_channel = 6"
 						elif send_channel == 7:
 							status_object.name = "Tray Engine Current"						
-							status_object.hardware_id = "hcboard_channel = 7"
+							status_object.hardware_id = "hwboard_channel = 7"
 
 					# evaluate error detection
 					if error_while_reading == 1:
 						level = 1
-						status_object.message = "detected error while receiving answer from hardware control board"
+						status_object.message = "detected error while receiving answer from hardware"
 
 					# append status object to publishing message
 
