749ec0820b3410172fc0f6673a88314e533b4547
ipa-fmw <florian.weisshardt@ipa.fraunhofer.de>
2013-01-10 17:09:26 +0100

    enable light marker by default

--- cob_light/ros/src/cob_light.cpp
+++ cob_light/ros/src/cob_light.cpp
@@ -220,7 +220,7 @@ class LightControl
 
 			_nh.param<std::string>("devicestring",_deviceString,"/dev/ttyLed");
 			_nh.param<int>("baudrate",_baudrate,230400);
-			_nh.param<bool>("pubmarker",_bPubMarker,false);
+			_nh.param<bool>("pubmarker",_bPubMarker,true);
 			
 			_sub = _nh.subscribe("command", 1, &LightControl::setRGB, this);
 
