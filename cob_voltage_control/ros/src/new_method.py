eb5af587819496f086be0d25fefab0521eb30098
Thiago de Freitas <thiagodefreitas@gmail.com>
2013-04-03 19:08:19 +0200

    Reverting new method

--- cob_voltage_control/ros/src/new_method.py
+++ cob_voltage_control/ros/src/new_method.py
@@ -1,4 +1,61 @@
 #!/usr/bin/env python
+
+#################################################################
+##\file
+#
+# \note
+# Copyright (c) 2013 \n
+# Fraunhofer Institute for Manufacturing Engineering
+# and Automation (IPA) \n\n
+#
+#################################################################
+#
+# \note
+# Project name: Care-O-bot Research
+# \note
+# ROS package name:
+#
+# \author
+# Author: Thiago de Freitas Oliveira Araujo,
+# email:thiago.de.freitas.oliveira.araujo@ipa.fhg.de
+# \author
+# Supervised by: Florian Weisshardt, email:florian.weisshardt@ipa.fhg.de
+#
+# \date Date of creation: April 2013
+#
+# \brief
+# Battery Characterization for the IPA robots
+#
+#################################################################
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# - Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer. \n
+# - Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution. \n
+# - Neither the name of the Fraunhofer Institute for Manufacturing
+# Engineering and Automation (IPA) nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission. \n
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License LGPL as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License LGPL for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License LGPL along with this program.
+# If not, see < http://www.gnu.org/licenses/>.
+#
+#################################################################
 import roslib; roslib.load_manifest('cob_relayboard')
 import rospy
 import time
@@ -57,15 +114,11 @@ class volts_filter():
         
         if(self.t_est <0):
             self.t_est = 0
-
-        rospy.loginfo(self.t_est/3600)
         
         self.msg_power.header.stamp = rospy.Time.now()
         self.msg_power.time_remaining.secs = self.t_est
-        self.msg_power.prediction_method = 'thiago_test'
-        self.msg_power.relative_capacity = (self.t_est/13500) * 100
-        
-        rospy.loginfo(self.volt_filt)
+        self.msg_power.prediction_method = '3rd_order_polynom'
+        self.msg_power.relative_capacity = (self.t_est/11748) * 100
         
         self.pub_power.publish(self.msg_power)
             
