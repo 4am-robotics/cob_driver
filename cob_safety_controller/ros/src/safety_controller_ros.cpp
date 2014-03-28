0b4d3f97c5902acf498b4eea2c29b108e5e0923a
Florian Weisshardt <florian.weisshardt@ipa.fraunhofer.de>
2014-03-28 16:53:26 +0100

    not yet ready

--- cob_safety_controller/ros/src/safety_controller_ros.cpp
+++ cob_safety_controller/ros/src/safety_controller_ros.cpp
@@ -4,6 +4,9 @@
 #include <cob_safety_controller/safety_controllerConfig.h>
 
 // ROS message includes
+#include <visualization_msgs/MarkerArray.h>
+#include <nav_msgs/Odometry.h>
+#include <cob_srvs/SetInt.h>
 
 
 
@@ -13,36 +16,52 @@
 
 class safety_controller_ros
 {
-	public:
-		ros::NodeHandle n_;
-		
-		dynamic_reconfigure::Server<cob_safety_controller::safety_controllerConfig> server;
-  		dynamic_reconfigure::Server<cob_safety_controller::safety_controllerConfig>::CallbackType f;
+  public:
+  ros::NodeHandle n_;
+	
+  dynamic_reconfigure::Server<cob_safety_controller::safety_controllerConfig> server;
+  dynamic_reconfigure::Server<cob_safety_controller::safety_controllerConfig>::CallbackType f;
+	
+
+  ros::Publisher marker_;
 		
 
-		
+  ros::Subscriber odometry_;
+  ros::ServiceServer set_mode_;
 
-        
- 
-        safety_controller_data component_data_;
-        safety_controller_config component_config_;
-        safety_controller_impl component_implementation_;
+  safety_controller_data component_data_;
+  safety_controller_config component_config_;
+  safety_controller_impl component_implementation_;
 
-        safety_controller_ros()
-        {
-       	
-  			f = boost::bind(&safety_controller_ros::configure_callback, this, _1, _2);
-  			server.setCallback(f);
+  safety_controller_ros()
+  {
+    f = boost::bind(&safety_controller_ros::configure_callback, this, _1, _2);
+    server.setCallback(f);
         	
+        		std::string set_mode_remap;
+        		n_.param("set_mode_remap", set_mode_remap, (std::string)"set_mode");
+        		set_mode_ = n_.advertiseService<cob_srvs::SetInt::Request , cob_srvs::SetInt::Response>(set_mode_remap, boost::bind(&safety_controller_impl::callback_set_mode, &component_implementation_,_1,_2,component_config_));
         
+				marker_ = 	n_.advertise<visualization_msgs::MarkerArray>("marker", 1);
+					odometry_ =  n_.subscribe("odometry", 1, &safety_controller_ros::topicCallback_odometry, this);
   	
 
+				n_.param("port", component_config_.port, (std::string)"empty");
+				n_.param("host", component_config_.host, (std::string)"empty");
             
         }
+		
         
+        void topicCallback_odometry(const nav_msgs::Odometry::ConstPtr& msg)
+		{
+            component_data_.in_odometry = *msg;
+            
+        }
 		
 		void configure_callback(cob_safety_controller::safety_controllerConfig &config, uint32_t level) 
 		{
+				component_config_.port = config.port;
+				component_config_.host = config.host;
 		}
 
         void configure()
@@ -53,12 +72,15 @@ class safety_controller_ros
 
 	    void activate_all_output()
         {
+		    component_data_.out_marker_active = true;
         }
 
         void update()
         {
             activate_all_output();
             component_implementation_.update(component_data_, component_config_);
+			  if (component_data_.out_marker_active)
+				marker_.publish(component_data_.out_marker);
     
         }
  
@@ -73,7 +95,7 @@ int main(int argc, char** argv)
     node.configure();
 
 	
- 	ros::Rate loop_rate(10.0); // Hz
+ 	ros::Rate loop_rate(1/10.0); // Hz
 
 	while(node.n_.ok())
 	{
