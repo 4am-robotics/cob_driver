dece7c6261ede6b63c2a34166a55e3c8cf5da220
Alexander Bubeck <alexander.bubeck@ipa.fraunhofer.de>
2014-06-18 17:34:20 +0200

    small changes to remove warnings

--- cob_relayboard/ros/src/relayboard_sim.py
+++ cob_relayboard/ros/src/relayboard_sim.py
@@ -9,20 +9,19 @@ def relayboard_sim():
 	rospy.init_node('cob_relayboard_sim')
 
 	# emergency_stop topic
-	pub_em_stop = rospy.Publisher('/emergency_stop_state', EmergencyStopState)
+	pub_em_stop = rospy.Publisher('/emergency_stop_state', EmergencyStopState, queue_size=1)
 	msg_em = EmergencyStopState()
 	msg_em.emergency_button_stop = False
 	msg_em.scanner_stop = False
 	msg_em.emergency_state = 0
 
 	# power_board/voltage topic
-	pub_voltage = rospy.Publisher('/power_board/voltage', Float64)
+	pub_voltage = rospy.Publisher('/power_board/voltage', Float64, queue_size=1)
 	msg_voltage = Float64()
 	msg_voltage.data = 48.0 # in simulation battery is always full
 
 	while not rospy.is_shutdown():
 		pub_em_stop.publish(msg_em)
-		pub_power_board.publish(msg_power_board)
 		pub_voltage.publish(msg_voltage)
 		rospy.sleep(1.0)
 
