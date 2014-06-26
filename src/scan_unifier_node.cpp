9c0705791b3602a76c62eaafc7f02d07198182fb
Florian Mirus <frm@ipa.fraunhofer.de>
2014-06-26 15:01:16 +0200

    adjusted license header in cob_scan_unifier

--- src/scan_unifier_node.cpp
+++ src/scan_unifier_node.cpp
@@ -1,33 +1,60 @@
 /*!
- *****************************************************************
- * \file
- *
- * \note
- *   Copyright (c) 2011 \n
- *   Fraunhofer Institute for Manufacturing Engineering
- *   and Automation (IPA) \n\n
- *
- *   All rightes reserved. \n\n
- *
- *****************************************************************
- *
- * \note
- *   Repository name: cob_navigation
- * \note
- *   ROS package name: cob_scan_unifier
- *
- * \author
- *   Author: Florian Mirus, email:florian.mirus@ipa.fhg.de
- * \author
- *   Supervised by: Alexander Bubeck, email:alexander.bubeck@ipa.fhg.de
- *
- * \date Date of creation: January 2011
- * \date Last Modification: June 2014
- *
- * \brief
- *   Takes in several laser scans and publishes them as a single one
- *
- ****************************************************************/
+*****************************************************************
+* \file
+*
+* \note
+* Copyright (c) 2014 \n
+* Fraunhofer Institute for Manufacturing Engineering
+* and Automation (IPA) \n\n
+*
+*****************************************************************
+*
+* \note
+* Repository name: cob_navigation
+* \note
+* ROS package name: cob_scan_unifier
+*
+* \author
+* Author: Florian Mirus, email:florian.mirus@ipa.fhg.de
+* \author
+* Supervised by: Alexander Bubeck, email:alexander.bubeck@ipa.fhg.de
+*
+* \date Date of creation: January 2011
+* \date Last Modification: June 2014
+*
+* \brief
+* Takes in several laser scans and publishes them as a single one
+*
+*****************************************************************
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* - Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer. \n
+* - Redistributions in binary form must reproduce the above copyright
+* notice, this list of conditions and the following disclaimer in the
+* documentation and/or other materials provided with the distribution. \n
+* - Neither the name of the Fraunhofer Institute for Manufacturing
+* Engineering and Automation (IPA) nor the names of its
+* contributors may be used to endorse or promote products derived from
+* this software without specific prior written permission. \n
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License LGPL as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License LGPL for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License LGPL along with this program.
+* If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************/
 
 #include <scan_unifier_node.h>
 
