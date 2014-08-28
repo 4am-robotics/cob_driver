e5301388aae10c17d107aa8eddf842a5fe9266e4
Denis Štogl <denis.stogl@kit.edu>
2014-08-28 17:21:15 +0200

    Voltage control working properly on raw3-5

--- cob_voltage_control/ros/src/cob_voltage_control_ros.cpp
+++ cob_voltage_control/ros/src/cob_voltage_control_ros.cpp
@@ -23,9 +23,10 @@ class cob_voltage_control_ros
         ros::NodeHandle n_;
 
         ros::Publisher topicPub_em_stop_state_;
-        ros::Publisher topicPub_powerstate;
-
+        ros::Publisher topicPub_power_board_state;
         ros::Publisher topicPub_Voltage;
+	ros::Publisher topicPub_PowerState;
+	
         ros::Subscriber topicSub_AnalogInputs;
         ros::Subscriber topicSub_DigitalInputs;
 
@@ -47,25 +48,24 @@ class cob_voltage_control_ros
 
         cob_voltage_control_ros()
         {
-            topicPub_powerstate = n_.advertise<cob_msgs::PowerBoardState>("pub_em_stop_state_", 1);
-            topicPub_em_stop_state_ = n_.advertise<cob_msgs::EmergencyStopState>("pub_relayboard_state_", 1);
-
+            topicPub_power_board_state = n_.advertise<cob_msgs::PowerBoardState>("pub_relayboard_state_", 1);    
+            topicPub_em_stop_state_ = n_.advertise<cob_msgs::EmergencyStopState>("pub_em_stop_state_", 1);
             topicPub_Voltage = n_.advertise<std_msgs::Float64>("/power_board/voltage", 10);
+// 	    topicPub_PowerState = n_.advertise<cob_msgs::PowerState>("/power_state", 1);
+	    
             topicSub_AnalogInputs = n_.subscribe("/analog_sensors", 10, &cob_voltage_control_ros::analogPhidgetSignalsCallback, this);
             topicSub_DigitalInputs = n_.subscribe("/digital_sensors", 10, &cob_voltage_control_ros::digitalPhidgetSignalsCallback, this);
                 
             n_.param("battery_max_voltage", component_config_.max_voltage, 50.0);
             n_.param("battery_min_voltage", component_config_.min_voltage, 44.0);
-            n_.param("robot_max_voltage", component_config_.max_voltage_res, 70.0);
-            n_.param("voltage_analog_port", component_config_.num_voltage_port, 1);
-            n_.param("em_stop_dio_port", component_config_.num_em_stop_port, 0); 
-            n_.param("scanner_stop_dio_port", component_config_.num_scanner_em_port, 1);
+            n_.param("robot_max_voltage", component_config_.max_voltage_res, 60.0);
+	    n_.param("phidget_max_counts", component_config_.max_counts, 1024); 
         
             last_rear_em_state = false;
             last_front_em_state = false;
 
             EM_stop_status_ = ST_EM_ACTIVE;
-            component_data_.out_pub_relayboard_state.scanner_stop = false;
+            component_data_.out_pub_em_stop_state_.scanner_stop = false;
         }
 
         void configure()
@@ -77,112 +77,102 @@ class cob_voltage_control_ros
         {
             component_implementation_.update(component_data_, component_config_);
             topicPub_Voltage.publish(component_data_.out_pub_voltage);
-            topicPub_powerstate.publish(component_data_.out_pub_em_stop_state_);
-            topicPub_em_stop_state_.publish(component_data_.out_pub_relayboard_state);
+// 	    topicPub_PowerState.publish(component_data_.out_pub_powerstate_);
+            topicPub_power_board_state.publish(component_data_.out_pub_power_board_state);
+            topicPub_em_stop_state_.publish(component_data_.out_pub_em_stop_state_);
         }
         
         void analogPhidgetSignalsCallback(const cob_phidgets::AnalogSensorConstPtr &msg)
         {
+	    int voltageState = -1;
+	    
             for(int i = 0; i < msg->uri.size(); i++)
             {
                 if( msg->uri[i] == "bat1")
                 {
-                    component_data_.in_phidget_voltage = msg->value[i];
+                    voltageState = msg->value[i];
                     break;
                 }
             }
+            // Calculatio of real voltage
+            double voltage = voltageState * component_config_.max_voltage_res/component_config_.max_counts;
+            component_data_.out_pub_voltage.data = voltage;
+	    
+	    // Linear calculatio for percentag and PowerState,
+	    double percentage =  (voltage - component_config_.min_voltage) * 100/(component_config_.max_voltage - component_config_.min_voltage);
+	    
+	    component_data_.out_pub_powerstate_.header.stamp = ros::Time::now();
+	    component_data_.out_pub_powerstate_.power_consumption = 0.0;
+	    component_data_.out_pub_powerstate_.time_remaining = ros::Duration(1000);
+	    component_data_.out_pub_powerstate_.relative_capacity = percentage;
         }
 
         void digitalPhidgetSignalsCallback(const cob_phidgets::DigitalSensorConstPtr &msg)
         {
-            bool front_em_active = false;
-            bool rear_em_active = false;
-            static bool em_caused_by_button = false;
+            bool em_scanner_active = false;
+            bool em_butt_active = false;
             cob_msgs::EmergencyStopState EM_msg;
             bool EM_signal = false;
             bool got_message = false;
 
             for(int i = 0; i < msg->uri.size(); i++)
             {
-                if( msg->uri[i] == "em_stop_laser_rear")
+                if( msg->uri[i] == "em_butt_not_stop")
                 {
-                    rear_em_active = !((bool)msg->state[i]);
+                    em_butt_active = !((bool)msg->state[i]);
                     got_message = true;
                 }
-                else if( msg->uri[i] == "em_stop_laser_front")
+                else if( msg->uri[i] == "em_scanner_not_stop")
                 {
-                    front_em_active = !((bool)msg->state[i]);
+                    em_scanner_active = !((bool)msg->state[i]);
                     got_message = true;
                 }
             }
             if(got_message)
             {
-				if( (front_em_active && rear_em_active) && (!last_front_em_state && !last_rear_em_state))
+		component_data_.out_pub_em_stop_state_.emergency_button_stop = em_butt_active;
+		component_data_.out_pub_em_stop_state_.scanner_stop = em_scanner_active;
+		
+		EM_signal = (bool)(em_butt_active | em_scanner_active);
+		
+		switch (EM_stop_status_)
+		{
+			case ST_EM_FREE:
+			{
+				if (EM_signal == true)
 				{
-					component_data_.out_pub_relayboard_state.emergency_button_stop = true;
-					em_caused_by_button = true;
+					ROS_INFO("Emergency stop was issued");
+					EM_stop_status_ = EM_msg.EMSTOP;
 				}
-				else if((!front_em_active && !rear_em_active) && (last_front_em_state && last_rear_em_state))
+				break;
+			}
+			case ST_EM_ACTIVE:
+			{
+				if (EM_signal == false)
 				{
-					component_data_.out_pub_relayboard_state.emergency_button_stop = false;
-					em_caused_by_button = false;
+					ROS_INFO("Emergency stop was confirmed");
+					EM_stop_status_ = EM_msg.EMCONFIRMED;
 				}
-				else if((front_em_active != rear_em_active) && em_caused_by_button)
+				break;
+			}
+			case ST_EM_CONFIRMED:
+			{
+				if (EM_signal == true)
 				{
-					component_data_.out_pub_relayboard_state.emergency_button_stop = false;
-					em_caused_by_button = false;
-					component_data_.out_pub_relayboard_state.scanner_stop = (bool)(front_em_active | rear_em_active);
+					ROS_INFO("Emergency stop was issued");
+					EM_stop_status_ = EM_msg.EMSTOP;
 				}
 				else
 				{
-					component_data_.out_pub_relayboard_state.scanner_stop = (bool)(front_em_active | rear_em_active);
-					ROS_INFO_STREAM("scanner_stop: "<<component_data_.out_pub_relayboard_state.scanner_stop);
+					ROS_INFO("Emergency stop released");
+					EM_stop_status_ = EM_msg.EMFREE;
 				}
-
-				EM_signal = component_data_.out_pub_relayboard_state.scanner_stop | component_data_.out_pub_relayboard_state.emergency_button_stop;
-
-				switch (EM_stop_status_)
-				{
-					case ST_EM_FREE:
-					{
-						if (EM_signal == true)
-						{
-							ROS_INFO("Emergency stop was issued");
-							EM_stop_status_ = EM_msg.EMSTOP;
-						}
-						break;
-					}
-					case ST_EM_ACTIVE:
-					{
-						if (EM_signal == false)
-						{
-							ROS_INFO("Emergency stop was confirmed");
-							EM_stop_status_ = EM_msg.EMCONFIRMED;
-						}
-						break;
-					}
-					case ST_EM_CONFIRMED:
-					{
-						if (EM_signal == true)
-						{
-							ROS_INFO("Emergency stop was issued");
-							EM_stop_status_ = EM_msg.EMSTOP;
-						}
-						else
-						{
-							ROS_INFO("Emergency stop released");
-							EM_stop_status_ = EM_msg.EMFREE;
-						}
-						break;
-					}
-				};
-
-
-				component_data_.out_pub_relayboard_state.emergency_state = EM_stop_status_;
-
-				last_front_em_state = front_em_active;
-				last_rear_em_state = rear_em_active;
+				break;
 			}
+		};
+
+		component_data_.out_pub_em_stop_state_.emergency_state = EM_stop_status_;
+	    }
         }
 };
 
