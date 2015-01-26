33aced54bae3265f6a565c7447ad488f10613dff
Thorsten Kannacher <thorsten.andreas.kannacher@ipa.fraunhofer.de>
2015-01-26 10:55:56 +0100

    modified relayboard_sim: now able to toggle each variable seperately by ros services

--- cob_relayboard/ros/src/relayboard_sim.py
+++ cob_relayboard/ros/src/relayboard_sim.py
@@ -5,33 +5,68 @@ import time
 from cob_msgs.msg import EmergencyStopState
 from cob_msgs.msg import PowerBoardState
 from std_msgs.msg import Float64
+from std_srvs.srv import Empty
 
-def relayboard_sim():
-	rospy.init_node('cob_relayboard_sim')
+class RelayboardSim:
 
-	# emergency_stop topic
+	# emergency_stop_state topic
 	pub_em_stop = rospy.Publisher('/emergency_stop_state', EmergencyStopState, queue_size=1)
 	msg_em = EmergencyStopState()
-	msg_em.emergency_button_stop = False
-	msg_em.scanner_stop = False
-	msg_em.emergency_state = 0
 
 	# power_board/state topic
 	pub_power_board = rospy.Publisher('/power_board/state', PowerBoardState, queue_size=1)
 	msg_power_board = PowerBoardState()
-	msg_power_board.header.stamp = rospy.Time.now()
-	msg_power_board.run_stop = True
-	msg_power_board.wireless_stop = True #for cob the wireless stop field is misused as laser stop field
 
 	# power_board/voltage topic
 	pub_voltage = rospy.Publisher('/power_board/voltage', Float64, queue_size=1)
 	msg_voltage = Float64()
-	msg_voltage.data = 48.0 # in simulation battery is always full
+
+	def __init__(self):
+
+		self.msg_em.emergency_button_stop = False
+		self.msg_em.scanner_stop = False
+		self.msg_em.emergency_state = 0
+
+		self.msg_power_board.header.stamp = rospy.Time.now()
+		self.msg_power_board.run_stop = True
+		self.msg_power_board.wireless_stop = True #for cob the wireless stop field is misused as laser stop field
+
+		self.msg_voltage.data = 48.0 # in simulation battery is always full
+
+	def toggle_emergency_button_stop(self, req):
+		
+		self.msg_em.emergency_button_stop = not self.msg_em.emergency_button_stop
+		return ()
+
+	def toggle_scanner_stop(self, req):	
+
+		self.msg_em.scanner_stop = not self.msg_em.scanner_stop
+		return ()		
+
+	def toggle_emergency_state(self, req):
+
+		if self.msg_em.emergency_state == 0:
+			self.msg_em.emergency_state = 1
+		else:
+			self.msg_em.emergency_state = 0
+
+		return ()
+
+def relayboard_sim():
+
+	rospy.init_node('cob_relayboard_sim')
+
+	relayboard_sim = RelayboardSim()
+
+	srv = rospy.Service('/relayboard_sim/toggle_emergency_button_stop', Empty, relayboard_sim.toggle_emergency_button_stop)
+	srv = rospy.Service('/relayboard_sim/toggle_scanner_stop', Empty, relayboard_sim.toggle_scanner_stop)
+	srv = rospy.Service('/relayboard_sim/toggle_emergency_state', Empty, relayboard_sim.toggle_emergency_state)
 
 	while not rospy.is_shutdown():
-		pub_em_stop.publish(msg_em)
-		pub_power_board.publish(msg_power_board)
-		pub_voltage.publish(msg_voltage)
+
+		relayboard_sim.pub_em_stop.publish(relayboard_sim.msg_em)
+		relayboard_sim.pub_power_board.publish(relayboard_sim.msg_power_board)
+		relayboard_sim.pub_voltage.publish(relayboard_sim.msg_voltage)
 		rospy.sleep(1.0)
 
 if __name__ == '__main__':
