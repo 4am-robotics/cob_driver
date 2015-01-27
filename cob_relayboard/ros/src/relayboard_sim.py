45b1dd7cf2318623718ab876932d1fb5b2c20dcc
Thorsten Kannacher <thorsten.andreas.kannacher@ipa.fraunhofer.de>
2015-01-27 16:07:28 +0100

    just updated node structure

--- cob_relayboard/ros/src/relayboard_sim.py
+++ cob_relayboard/ros/src/relayboard_sim.py
@@ -37,19 +37,29 @@ class RelayboardSim:
 		self.msg_voltage.data = 48.0 # in simulation battery is always full
 
 		# advertise services
-		srv = rospy.Service('~toggle_emergency_button_stop', Empty, self.toggle_emergency_button_stop)
-		srv = rospy.Service('~toggle_scanner_stop', Empty, self.toggle_scanner_stop)
+		toggle_emergency_button_srv = rospy.Service('~toggle_emergency_button_stop', Empty, self.toggle_emergency_button_stop)
+		toggle_scanner_stop_srv = rospy.Service('~toggle_scanner_stop', Empty, self.toggle_scanner_stop)
 		
 	# service callback to simulate emergency button stop
 	def toggle_emergency_button_stop(self, req):
 		
 		self.msg_em.emergency_button_stop = not self.msg_em.emergency_button_stop
+
+		if self.msg_em.emergency_state != 1:
+			self.msg_em.emergency_state = 1
+			rospy.logerr("Emergency stop issued.")
+
 		return ()
 
 	# service callback to simulate laser scanner stop
 	def toggle_scanner_stop(self, req):	
 
 		self.msg_em.scanner_stop = not self.msg_em.scanner_stop
+		
+		if self.msg_em.emergency_state != 1:
+			self.msg_em.emergency_state = 1
+			rospy.logerr("Emergency stop issued.")
+			
 		return ()		
 
 def relayboard_sim():
@@ -59,37 +69,17 @@ def relayboard_sim():
 	relayboard_sim = RelayboardSim()
 
 	while not rospy.is_shutdown():
-
-		# initial situation: system operating normal
-		if relayboard_sim.msg_em.emergency_state == 0:
-
-			if relayboard_sim.msg_em.emergency_button_stop == 1 or relayboard_sim.msg_em.scanner_stop == 1:
-				relayboard_sim.msg_em.emergency_state = 1
-				rospy.logerr("Emergency stop issued.")
-
-		# initial situation: emergency stop was issued
-		if relayboard_sim.msg_em.emergency_state == 1:
-
-			if relayboard_sim.msg_em.emergency_button_stop == 0 and relayboard_sim.msg_em.scanner_stop == 0:
-				relayboard_sim.msg_em.emergency_state = 2 # emergency stop was confirmed
-				rospy.loginfo("Emergency stop confirmed.")
-
-		if relayboard_sim.msg_em.emergency_state == 2:
-
-			if relayboard_sim.msg_em.emergency_button_stop == 1 or relayboard_sim.msg_em.scanner_stop == 1:
-				relayboard_sim.msg_em.emergency_state = 1
-				rospy.logerr("Emergency stop issued.")
-
-			else:
-				relayboard_sim.msg_em.emergency_state = 0
-				rospy.loginfo("Emergency stop released.")
+		
+		if relayboard_sim.msg_em.emergency_state == 1 and relayboard_sim.msg_em.emergency_button_stop == 0 and relayboard_sim.msg_em.scanner_stop == 0:
+			relayboard_sim.msg_em.emergency_state = 0
+			rospy.loginfo("Emergency stop released")
 	
 		# publish topics
 		relayboard_sim.pub_em_stop.publish(relayboard_sim.msg_em)
 		relayboard_sim.pub_power_board.publish(relayboard_sim.msg_power_board)
 		relayboard_sim.pub_voltage.publish(relayboard_sim.msg_voltage)
 
-		rospy.sleep(0.5)
+		rospy.sleep(1)
 
 if __name__ == '__main__':
 	
