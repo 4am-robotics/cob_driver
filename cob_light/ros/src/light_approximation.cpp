36ad0dceec964d44ed559f962be99eed68e98299
bnm <bnm@ipa.fhg.de>
2015-10-14 20:31:42 +0200

    wip light_approximation

--- cob_light/ros/src/light_approximation.cpp
+++ cob_light/ros/src/light_approximation.cpp
@@ -5,8 +5,10 @@
 #include <colorUtils.h>
 
 #include <algorithm>
+#include <limits>
 
 #include <boost/thread.hpp>
+#include <boost/algorithm/clamp.hpp>
 
 sensor_msgs::LaserScan scan;
 boost::mutex mutex;
@@ -15,6 +17,7 @@ cob_light::LightMode mode_msg;
 static const int NUM_LEDS = 58;
 color::rgba c_red;
 color::rgba c_green;
+color::rgba c_off;
 
 float linearInterpolate(float a, float b, float t)
 {
@@ -59,23 +62,30 @@ void scan_callback(const sensor_msgs::LaserScanConstPtr& msg)
 void timerCallback(const ros::TimerEvent& event)
 {
     //rotate scan array
+    mutex.lock();
     std::vector<float> ranges = scan.ranges;
-    std::rotate(ranges.begin(), ranges.begin()+ranges.size()/2, ranges.end());
-    int sector_size = ranges.size() / NUM_LEDS;
+    mutex.unlock();
+    //std::rotate(ranges.begin(), ranges.begin()+ranges.size()/2, ranges.end());
+    float sector_size = (float)ranges.size() / (float)NUM_LEDS;
+    //ROS_INFO_STREAM("sector_size: "<<sector_size);
+    //ROS_INFO_STREAM("ranges size: "<<ranges.size());
     std::vector<float> sectors;
     sectors.assign(NUM_LEDS, 0.0);
 
     //calculate sector values
     for(int i = 0; i < NUM_LEDS; i++)
     {
-        float sum = 0;
-        for(int j = i*sector_size; j < (i+1)*sector_size; j++)
+	int num = 0;
+        float sum = std::numeric_limits<float>::max();
+	//float sum = 0;
+        for(int j = i*(int)sector_size; j < (i+1)*(int)sector_size; j++)
         {
-            sum += ranges.at(j);
+	    if(ranges.at(j) != 0){
+            	sum = std::min(ranges.at(j), sum);
+	    }
         }
-        sectors.at(i) = sum / sector_size;
-	
-	ROS_INFO_STREAM("sector "<<i<<": "<<sectors.at(i));
+        sectors.at(i) = sum;
+	//ROS_INFO_STREAM("Sektor "<<i<<": "<<sectors.at(i));
     }
 
     for(int i = 0; i < NUM_LEDS; i++)
@@ -83,30 +93,39 @@ void timerCallback(const ros::TimerEvent& event)
         std_msgs::ColorRGBA color;
         color::rgba col;
 
-        if(sectors.at(i) > 2.5)
-            col = c_green;
-        else if(sectors.at(i) < 0.3)
-            col = c_red;
+        float mean = 0;
+        if(i == 0)
+        {
+            mean = sectors.back()+sectors.at(i)/2.0f;
+        }
         else
         {
-            float mean = 0;
-            if(i == 0)
-                mean = sectors.back()+sectors.at(i)/2.0f;
-            else
-                mean = (sectors.at(i)+sectors.at(i-1))/2.0f;
-
-            float t = (mean - 0.3)/(2.5 - 0.3);
-            col = interpolateColor(c_red, c_green, t);
+            mean = (sectors.at(i)+sectors.at(i-1))/2.0f;
         }
+
+        float t = (boost::algorithm::clamp(mean, 0.6, 2.0) - 0.6)/(2.0 - 0.6);
+        col = interpolateColor(c_red, c_green, t);
+       
         color.a = col.a;
         color.r = col.r;
         color.g = col.g;
         color.b = col.b;
         mode_msg.colors.at(i) = color;
-	mode_msg.colors.at(NUM_LEDS/2).a = 1;
-	mode_msg.colors.at(NUM_LEDS/2).r = 1;
-	mode_msg.colors.at(NUM_LEDS/2).g = 0;
-	mode_msg.colors.at(NUM_LEDS/2).b = 0;
+
+	//mode_msg.colors.at(0).a = 1;
+	//mode_msg.colors.at(0).r = 1;
+	//mode_msg.colors.at(0).g = 0;
+	//mode_msg.colors.at(0).b = 0;
+
+	//mode_msg.colors.at(5).a = 1;
+	//mode_msg.colors.at(5).r = 0;
+	//mode_msg.colors.at(5).g = 0;
+	//mode_msg.colors.at(5).b = 1;
+
+	//mode_msg.colors.at(25).a = 1;
+	//mode_msg.colors.at(25).r = 1;
+	//mode_msg.colors.at(25).g = 0;
+	//mode_msg.colors.at(25).b = 0;
     }
     pubLight.publish(mode_msg);
 }
@@ -118,7 +137,7 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
   ros::Subscriber sub_scan = nh.subscribe("/scan_unified", 1, scan_callback);
   pubLight = nh.advertise<cob_light::LightMode>("/light_torso/light", 1);
-  ros::Timer timer = nh.createTimer(ros::Duration(0.05), timerCallback);
+  ros::Timer timer = nh.createTimer(ros::Duration(0.025), timerCallback);
   std_msgs::ColorRGBA color;
   mode_msg.colors.assign(NUM_LEDS, color);
   mode_msg.priority = 1;
@@ -126,6 +145,7 @@ int main(int argc, char** argv)
 
   c_red.a = 1; c_red.r = 1; c_red.g = 0; c_red.b = 0;
   c_green.a = 1; c_green.r = 0; c_green.g = 1; c_green.b = 0;
+  c_off.a = 0; c_off.r = 0; c_off.g = 0; c_off.b = 0;
 
   ros::spin();
 
