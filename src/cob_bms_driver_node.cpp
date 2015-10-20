a82f2fdb47f06df5ec077dacac04de8d6bddddd3
ipa-mig-mc <muhammad.chughtai@ipa.fraunhofer.de>
2015-10-20 16:50:00 +0200

    created hardcoded parameter lists

--- src/cob_bms_driver_node.cpp
+++ src/cob_bms_driver_node.cpp
@@ -4,13 +4,14 @@
 #include "socketcan_interface/socketcan.h"
 #include "socketcan_interface/threading.h"
 #include "socketcan_interface/string.h"
+#include <vector>
 
 
 can::ThreadedSocketCANInterface driver_;
 
 void handleFrames(const can::Frame &f){
     // handle all frames
-    LOG("got: " << can::tostring(f, true
+    LOG("got: " << can::tostring(f, true));
 }
 
 /*void handleFrames(const can::Frame &f) {
@@ -18,10 +19,13 @@ void handleFrames(const can::Frame &f){
 	LOG("123? " << can::tostring(f, true)); // lowercase: true
 }*/
 
+std::vector<std::string> paramater_ids_list1_;
+std::vector<std::string> paramater_ids_list2_;
+
 //function that polls all batteries (i.e. at CAN ID: 0x200) for two parameters at a time
 bool pollBMSforCANids(const std::string first_parameter_id, const std::string second_parameter_id) {
 	
-	strd::string msg = "200#"+first_parameter_id+second_parameter_id;
+	std::string msg = "200#"+first_parameter_id+second_parameter_id;
 	driver_.send(can::toframe(msg)); 
 	ROS_INFO_STREAM("sending message: " << msg);
 	boost::this_thread::sleep_for(boost::chrono::milliseconds(25));
@@ -32,6 +36,12 @@ bool pollBMSforCANids(const std::string first_parameter_id, const std::string se
 	
 int main(int argc, char **argv) {
 	
+	//hardcoded parameter lists 
+	paramater_ids_list1_.push_back("0101"); paramater_ids_list1_.push_back("0102"); paramater_ids_list1_.push_back("0106");
+	paramater_ids_list2_.push_back("0115"); paramater_ids_list2_.push_back("0116"); paramater_ids_list2_.push_back("0117"); 
+	paramater_ids_list2_.push_back("0118"); paramater_ids_list2_.push_back("0119"); paramater_ids_list2_.push_back("011A");
+	paramater_ids_list2_.push_back("011B");
+	
 	if(!driver_.init("can0", false)) return 1; // read own messages: false
 	
 	can::CommInterface::FrameListener::Ptr all_frames = driver_.createMsgListener(handleFrames);
@@ -41,10 +51,16 @@ int main(int argc, char **argv) {
 	ros::NodeHandle n;
 	//ros::Publisher canbus_pub = n.advertise<std_msgs::String>("canbus", 1000);
 	
+	size_t i = paramater_ids_list1_.size();
+	size_t j = paramater_ids_list2_.size();
+	
 	while (ros::ok())
     {
 	
-		pollBMSforCANids("0101", "0105");
+		pollBMSforCANids(paramater_ids_list1_.at(i), paramater_ids_list2_.at(j));
+		
+		(i==0) ? i = paramater_ids_list1_.size() : --i;
+		(j==0) ? j = paramater_ids_list2_.size() : --j;	
   
 		ros::spinOnce();
     }
