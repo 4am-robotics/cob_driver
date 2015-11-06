08e90dbeb7afdbe6ebbdd57fd1042bbec9ecfa04
ipa-mig-mc <muhammad.chughtai@ipa.fraunhofer.de>
2015-11-06 17:29:02 +0100

    added missing file

---
+++ include/cob_bms_driver/bms_parameter.h
@@ -0,0 +1,24 @@
+#ifndef BMS_PARAMETER_H
+#define BMS_PARAMETER_H
+
+struct BmsParameter
+{	
+	//struct Address 
+	//{
+		//int can_id;
+		unsigned int offset;
+		unsigned int length;
+	//}
+	
+	//struct Metadata 
+	//{
+		std::string name;
+		bool is_signed;
+		double factor;
+		std::string unit;
+	//}
+	
+	double data;
+};
+
+#endif
