36cca3e33128ab90538061c57fbd74bd258d02a0
Denis Štogl <denis.stogl@kit.edu>
2016-02-15 12:22:25 +0100

    Licence update and formating.

--- cob_generic_can/common/include/cob_generic_can/SocketCan.h
+++ cob_generic_can/common/include/cob_generic_can/SocketCan.h
@@ -1,19 +1,39 @@
 /*****************************************************************************
- * Copyright 2015 Intelligent Industrial Robotics (IIROB) Group,
+ *
+ * Copyright 2016 Intelligent Industrial Robotics (IIROB) Group,
  * Institute for Anthropomatics and Robotics (IAR) -
  * Intelligent Process Control and Robotics (IPR),
- * Karlsruhe Institute of Technology
-
+ * Karlsruhe Institute of Technology (KIT)
+ *
+ * Author: Denis Štogl, email: denis.stogl@kit.edu
+ *
+ * Date of creation: February 2016
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
  * This package is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
-
+ *
  * This package is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
-
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this package. If not, see <http://www.gnu.org/licenses/>.
 *****************************************************************************/
@@ -28,40 +48,40 @@
 class SocketCan : public CanItf
 {
 public:
-	// --------------- Interface
-	SocketCan(const char* device, int baudrate);
-	SocketCan(const char* device);
-	~SocketCan();
-	bool init_ret();
-	void init();
-	void destroy() {};
-	bool transmitMsg(CanMsg CMsg, bool bBlocking = true);
-	bool receiveMsg(CanMsg* pCMsg);
-	bool receiveMsgRetry(CanMsg* pCMsg, int iNrOfRetry);
-	bool receiveMsgTimeout(CanMsg* pCMsg, int nMicroSeconds);
-	bool isObjectMode() { return false; }
+    // --------------- Interface
+    SocketCan ( const char* device, int baudrate );
+    SocketCan ( const char* device );
+    ~SocketCan();
+    bool init_ret();
+    void init();
+    void destroy() {};
+    bool transmitMsg ( CanMsg CMsg, bool bBlocking = true );
+    bool receiveMsg ( CanMsg* pCMsg );
+    bool receiveMsgRetry ( CanMsg* pCMsg, int iNrOfRetry );
+    bool receiveMsgTimeout ( CanMsg* pCMsg, int nMicroSeconds );
+    bool isObjectMode() {
+        return false;
+    }
 
 private:
-	// --------------- Types
+    // --------------- Types
     can::SocketCANInterface m_handle;
     can::CommInterface::FrameListener::Ptr frame_printer;
 
-	bool m_bInitialized;
-	bool m_bSimuEnabled;
-	const char* p_cDevice;
-	int m_iBaudrateVal;
+    bool m_bInitialized;
+    bool m_bSimuEnabled;
+    const char* p_cDevice;
+    int m_iBaudrateVal;
 
-	std::list<can::Frame> recived_frame;
+    std::list<can::Frame> recived_frame;
 
-	static const int c_iInterrupt;
-	static const int c_iPort;
+    static const int c_iInterrupt;
+    static const int c_iPort;
 
-	bool initCAN();
+    bool initCAN();
 
-	void recive_frame(const can::Frame & frame);
-	void print_error(const can::State & state);
+    void recive_frame ( const can::Frame& frame );
+    void print_error ( const can::State& state );
 };
 //-----------------------------------------------
 #endif
-
-
--- cob_generic_can/common/src/SocketCan.cpp
+++ cob_generic_can/common/src/SocketCan.cpp
@@ -1,19 +1,39 @@
 /*****************************************************************************
- * Copyright 2015 Intelligent Industrial Robotics (IIROB) Group,
+ *
+ * Copyright 2016 Intelligent Industrial Robotics (IIROB) Group,
  * Institute for Anthropomatics and Robotics (IAR) -
  * Intelligent Process Control and Robotics (IPR),
- * Karlsruhe Institute of Technology
-
+ * Karlsruhe Institute of Technology (KIT)
+ *
+ * Author: Denis Štogl, email: denis.stogl@kit.edu
+ *
+ * Date of creation: February 2016
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Redistribution and use in source and binary forms,
+ * with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
  * This package is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
-
+ *
  * This package is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
-
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this package. If not, see <http://www.gnu.org/licenses/>.
 *****************************************************************************/
@@ -30,56 +50,56 @@
 
 SocketCan::SocketCan(const char* device, int baudrate)
 {
-        m_bInitialized = false;
+    m_bInitialized = false;
 
-        p_cDevice = device;
-        can::SocketCANInterface m_handle;
+    p_cDevice = device;
+    can::SocketCANInterface m_handle;
 }
 
 SocketCan::SocketCan(const char* device)
 {
-        m_bInitialized = false;
+    m_bInitialized = false;
 
-        p_cDevice = device;
-        can::SocketCANInterface m_handle;
+    p_cDevice = device;
+    can::SocketCANInterface m_handle;
 }
 
 //-----------------------------------------------
 SocketCan::~SocketCan()
 {
-        if (m_bInitialized)
-        {
-               m_handle.shutdown(); // welche aufgabe muss erledigt werden wenn dekonstrulieren
-        }
+    if (m_bInitialized)
+    {
+        m_handle.shutdown(); // welche aufgabe muss erledigt werden wenn dekonstrulieren
+    }
 }
 
 //-----------------------------------------------
 bool SocketCan::init_ret()
 {
-        bool ret = true;
-        // init() - part
-        if (!m_handle.init(p_cDevice, false))
-        {
-                print_error(m_handle.getState());
-                ret = false;
-        }
-        else
-        {
-                ret = initCAN();
-                frame_printer = m_handle.createMsgListener(can::CommInterface::FrameDelegate(this, &SocketCan::recive_frame));
-                m_handle.run();
-        }
-        return ret;
+    bool ret = true;
+    // init() - part
+    if (!m_handle.init(p_cDevice, false))
+    {
+        print_error(m_handle.getState());
+        ret = false;
+    }
+    else
+    {
+        ret = initCAN();
+        frame_printer = m_handle.createMsgListener(can::CommInterface::FrameDelegate(this, &SocketCan::recive_frame));
+        m_handle.run();
+    }
+    return ret;
 }
 
 //-----------------------------------------------
 void SocketCan::init()
 {
-        if (!init_ret())
-        {
-                sleep(3);
-                exit(0);
-        }
+    if (!init_ret())
+    {
+        sleep(3);
+        exit(0);
+    }
 }
 
 
@@ -88,31 +108,37 @@ bool SocketCan::transmitMsg(CanMsg CMsg, bool bBlocking)
 {
     can::Header header(CMsg.getID(), false, false, false);
     can::Frame message(header, CMsg.getLength());
-    for(int i=0; i<CMsg.getLength(); i++)
+    for (int i = 0; i < CMsg.getLength(); i++)
+    {
         message.data[i] = CMsg.getAt(i);
+    }
     return m_handle.send(message);
 }
 
 //-------------------------------------------
 bool SocketCan::receiveMsg(CanMsg* pCMsg)
 {
-    if (m_bInitialized == false) return false;
+    if (m_bInitialized == false)
+    {
+        return false;
+    }
 
     bool bRet = false;
     can::Frame frame;
 
-    if(recived_frame.size())
+    if (recived_frame.size())
     {
         frame = recived_frame.front();
         recived_frame.pop_front();
-        std::cout<<"length:"<<frame.dlc<<std::endl;
+        std::cout << "length:" << frame.dlc << std::endl;
         pCMsg->setID(frame.id);
         pCMsg->setLength(frame.dlc);
         pCMsg->set(frame.data[0], frame.data[1], frame.data[2], frame.data[3],
-        		frame.data[4], frame.data[5], frame.data[6], frame.data[7]);
+                   frame.data[4], frame.data[5], frame.data[6], frame.data[7]);
         bRet = true;
     }
-    else {
+    else
+    {
         std::cout << "No message recieved: " << std::endl;
     }
     return bRet;
@@ -121,42 +147,49 @@ bool SocketCan::receiveMsg(CanMsg* pCMsg)
 //-------------------------------------------
 bool SocketCan::receiveMsgRetry(CanMsg* pCMsg, int iNrOfRetry)
 {
-    if (m_bInitialized == false) return false;
+    if (m_bInitialized == false)
+    {
+        return false;
+    }
 
     can::Frame frame;
     bool bRet = false;
-    int i=0;
+    int i = 0;
 
     do
     {
-        if(recived_frame.size())
+        if (recived_frame.size())
         {
             frame = recived_frame.front();
             recived_frame.pop_front();
             pCMsg->setID(frame.id);
             pCMsg->setLength(frame.dlc);
             pCMsg->set(frame.data[0], frame.data[1], frame.data[2], frame.data[3],
-                        frame.data[4], frame.data[5], frame.data[6], frame.data[7]);
+                       frame.data[4], frame.data[5], frame.data[6], frame.data[7]);
             bRet = true;
             break;
         }
         i++;
         usleep(1000);
     }
-    while((i < iNrOfRetry && bRet!=true));
+    while ((i < iNrOfRetry && bRet != true));
     return bRet;
 }
 
 //-------------------------------------------
 bool SocketCan::receiveMsgTimeout(CanMsg* pCMsg, int nSecTimeout)
 {
-    if (m_bInitialized == false) return false;
+    if (m_bInitialized == false)
+    {
+        return false;
+    }
 
     bool bRet = false;
     can::Frame frame;
-    usleep(nSecTimeout/1000);
+    usleep(nSecTimeout / 1000);
 
-    if(recived_frame.size() > 0){
+    if (recived_frame.size() > 0)
+    {
         frame = recived_frame.front();
         recived_frame.pop_front();
         pCMsg->setID(frame.id);
@@ -168,15 +201,18 @@ bool SocketCan::receiveMsgTimeout(CanMsg* pCMsg, int nSecTimeout)
     return bRet;
 }
 
-bool SocketCan::initCAN() {
+bool SocketCan::initCAN()
+{
     m_bInitialized = true;
     bool bRet = true;
     return true;
 }
 
-void SocketCan::recive_frame(const can::Frame & frame){
+void SocketCan::recive_frame(const can::Frame& frame)
+{
 
-    if(frame.is_error){
+    if (frame.is_error)
+    {
         can::State state;
         print_error(state);
     }
@@ -186,9 +222,10 @@ void SocketCan::recive_frame(const can::Frame & frame){
     }
 }
 
-void SocketCan::print_error(const can::State & state){
-     std::string err;
-     std::cout << "ERROR: state=" << std::endl;
+void SocketCan::print_error(const can::State& state)
+{
+    std::string err;
+    std::cout << "ERROR: state=" << std::endl;
     m_handle.translateError(state.internal_error, err);
     std::cout << "ERROR: state=" << state.driver_state << " internal_error=" << state.internal_error << "('" << err << "') asio: " << state.error_code << std::endl;
 }
