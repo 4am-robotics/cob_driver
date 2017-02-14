6029a493c931fd8f5f2c763046b6715f32b7e56c
fmw-ss <sourav.ranjan.senapati@ipa.fraunhofer.de>
2017-02-14 16:43:50 +0100

    consistent naming

--- cob_bms_driver/src/fake_bms.py
+++ cob_bms_driver/src/fake_bms.py
@@ -10,21 +10,21 @@ from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus
 class FakeBMS(object):
     def __init__(self):
         self.srv_charging = rospy.Service('~set_charging', SetBool, self.charging_cb)
-        self.srv_relative_remaining_capacity = rospy.Service('~set_relative_remaining_capacity', SetFloat, self.relative_remaining_capacity_cb)
-        self.poll_frequency         = rospy.get_param('~poll_frequency_hz', 20.0)
-        self.pub_voltage            = rospy.Publisher('~voltage', Float64, queue_size = 1)
-        self.pub_current            = rospy.Publisher('~current', Float64, queue_size = 1)
-        self.pub_remaining_capacity = rospy.Publisher('~remaining_capacity', Float64, queue_size = 1)
-        self.pub_charge_capacity    = rospy.Publisher('~full_charge_capacity', Float64, queue_size = 1)
-        self.pub_temparature        = rospy.Publisher('~temperature', Float64, queue_size = 1)
-        self.pub_charging_state     = rospy.Publisher('~battery_charging', Bool, queue_size = 1)        
+        self.srv_remaining_capacity   = rospy.Service('~set_remaining_capacity', SetFloat, self.remaining_capacity_cb)
+        self.poll_frequency           = rospy.get_param('~poll_frequency_hz', 20.0)
+        self.pub_voltage              = rospy.Publisher('~voltage', Float64, queue_size = 1)
+        self.pub_current              = rospy.Publisher('~current', Float64, queue_size = 1)
+        self.pub_remaining_capacity   = rospy.Publisher('~remaining_capacity', Float64, queue_size = 1)
+        self.pub_full_charge_capacity = rospy.Publisher('~full_charge_capacity', Float64, queue_size = 1)
+        self.pub_temparature          = rospy.Publisher('~temperature', Float64, queue_size = 1)
+        self.pub_charging_state       = rospy.Publisher('~battery_charging', Bool, queue_size = 1)        
         
-        self.voltage            = 0.0
-        self.current            = 0.0
-        self.remaining_capacity = 0.0
-        self.charge_capacity    = 0.0
-        self.temperature        = 0.0
-        self.charging_state     = False
+        self.voltage              = 0.0
+        self.current              = 0.0
+        self.remaining_capacity   = 0.0
+        self.full_charge_capacity = 1.0
+        self.temperature          = 0.0
+        self.charging_state       = False
 
         self._fake_diag_pub = rospy.Publisher('/diagnostics', DiagnosticArray, queue_size=1)
         rospy.Timer(rospy.Duration(1.0), self.publish_diagnostics)
@@ -36,9 +36,9 @@ class FakeBMS(object):
         res_charging = SetBoolResponse(True, "Set charging to {}".format(req.data))
         return res_charging
         
-    def relative_remaining_capacity_cb(self, req):
+    def remaining_capacity_cb(self, req):
         self.remaining_capacity = req.data
-        res_capacity = SetFloatResponse(True, "Set relative remaining capacity to {}".format(req.data))
+        res_capacity = SetFloatResponse(True, "Set remaining capacity to {}".format(req.data))
         return res_capacity
 
 
@@ -59,7 +59,7 @@ class FakeBMS(object):
         self.pub_voltage.publish(self.voltage)
         self.pub_current.publish(self.current)
         self.pub_remaining_capacity.publish(self.remaining_capacity)
-        self.pub_charge_capacity.publish(self.charge_capacity)
+        self.pub_full_charge_capacity.publish(self.full_charge_capacity)
         self.pub_temparature.publish(self.temperature)
         self.pub_charging_state.publish(self.charging_state)
         
