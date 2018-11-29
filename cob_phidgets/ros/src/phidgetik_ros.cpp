0b961c06b562728ed2dede5fe8cdcd936ca4d410
fmessmer <felixmessmer@gmail.com>
2018-11-29 09:19:13 +0100

    fix indentation

--- cob_phidgets/ros/src/phidgetik_ros.cpp
+++ cob_phidgets/ros/src/phidgetik_ros.cpp
@@ -297,38 +297,38 @@ auto PhidgetIKROS::setDigitalOutCallback(cob_phidgets::SetDigitalSensor::Request
 	_indexNameMapRevItr = _indexNameMapDigitalOutRev.find(req.uri);
 	if(_indexNameMapRevItr != _indexNameMapDigitalOutRev.end())
 	{
-        if(this->getOutputState(_indexNameMapDigitalOutRev[req.uri]) == req.state)
-        {
-            ROS_INFO("Digital output %i is already at state %i", _indexNameMapDigitalOutRev[req.uri], req.state);
-            res.uri = req.uri;
-            res.state = req.state;
-            ret = true;
-        }
-        else
-        {
-            ROS_INFO("Setting digital output %i to state %i", _indexNameMapDigitalOutRev[req.uri], req.state);
-            this->setOutputState(_indexNameMapDigitalOutRev[req.uri], req.state);
+		if(this->getOutputState(_indexNameMapDigitalOutRev[req.uri]) == req.state)
+		{
+			ROS_INFO("Digital output %i is already at state %i", _indexNameMapDigitalOutRev[req.uri], req.state);
+			res.uri = req.uri;
+			res.state = req.state;
+			ret = true;
+		}
+		else
+		{
+			ROS_INFO("Setting digital output %i to state %i", _indexNameMapDigitalOutRev[req.uri], req.state);
+			this->setOutputState(_indexNameMapDigitalOutRev[req.uri], req.state);
 
-            ros::Time start = ros::Time::now();
-            while((ros::Time::now().toSec() - start.toSec()) < 1.0)
-            {
-                _mutex.lock();
-                if(_outputChanged.updated == true)
-                {
-                    _mutex.unlock();
-                    break;
-                }
-                _mutex.unlock();
+			ros::Time start = ros::Time::now();
+			while((ros::Time::now().toSec() - start.toSec()) < 1.0)
+			{
+				_mutex.lock();
+				if(_outputChanged.updated == true)
+				{
+					_mutex.unlock();
+					break;
+				}
+				_mutex.unlock();
 
-                ros::Duration(0.025).sleep();
-            }
-            _mutex.lock();
-            res.uri = _indexNameMapDigitalOut[_outputChanged.index];
-            res.state = _outputChanged.state;
-            ROS_DEBUG("Sending response: updated: %u, index: %d, state: %d",_outputChanged.updated, _outputChanged.index, _outputChanged.state);
-            ret = (_outputChanged.updated && (_outputChanged.index == _indexNameMapDigitalOutRev[req.uri]));
-            _mutex.unlock();
-        }
+				ros::Duration(0.025).sleep();
+			}
+			_mutex.lock();
+			res.uri = _indexNameMapDigitalOut[_outputChanged.index];
+			res.state = _outputChanged.state;
+			ROS_DEBUG("Sending response: updated: %u, index: %d, state: %d",_outputChanged.updated, _outputChanged.index, _outputChanged.state);
+			ret = (_outputChanged.updated && (_outputChanged.index == _indexNameMapDigitalOutRev[req.uri]));
+			_mutex.unlock();
+		}
 	}
 	else
 	{
@@ -416,10 +416,10 @@ auto PhidgetIKROS::attachHandler() -> int
 auto PhidgetIKROS::detachHandler() -> int
 {
 	int serial_number;
-    const char *device_name;
+	const char *device_name;
 
-    CPhidget_getDeviceName ((CPhidgetHandle)_iKitHandle, &device_name);
-    CPhidget_getSerialNumber((CPhidgetHandle)_iKitHandle, &serial_number);
-    ROS_INFO("%s Serial number %d detached!", device_name, serial_number);
-    return 0;
+	CPhidget_getDeviceName ((CPhidgetHandle)_iKitHandle, &device_name);
+	CPhidget_getSerialNumber((CPhidgetHandle)_iKitHandle, &serial_number);
+	ROS_INFO("%s Serial number %d detached!", device_name, serial_number);
+	return 0;
 }
